09d72eb63c7f38f084dd8a5f669373e0d71fd87a
Dnyaneshwar ghodse <dnyaneshwarghodse@analytic-edge.com>
2023-01-20 13:02:44 +0530

    insert pop remove

--- LinkedList/SinglyLinked.js
+++ LinkedList/SinglyLinked.js
@@ -22,9 +22,69 @@ class SinglyLikedList{
         this.length++;
         return this
     }
+    prepend(value){
+        const newNode = new Node(value);
+        if(this.length == 0){
+            return this.append(value);
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+    insert(index, value){
+        if(index == 0){
+            return this.prepend(value);
+        }
+        if(index >= this.length){
+            return this.append(value);
+        }
+        const newNode = new Node(value);
+        const leader = this.traverseAndReturnIndexNode(index - 1);
+        const holdingPinter = leader.next.next;
+        leader.next =  newNode;
+        newNode.next = holdingPinter;
+        this.length++;
+        return this;
+    }
+    pop(){
+        let current = this.head;
+        let prev = this.head;
+        while(current.next){
+            prev = current;
+            current = current.next;
+        }
+        this.tail = prev;
+        this.tail.next = null;
+        this.length--;
+        return this;
+    }
+    remove(index){
+        if(index >= this.length){
+            return this.pop();
+        }
+        const leader = this.traverseAndReturnIndexNode(index - 1);
+        let holdingPointer = leader.next.next;
+        leader.next = holdingPointer;
+        this.length--;
+        return this;
+    }
+    traverseAndReturnIndexNode(index){
+        let count = 0;
+        let indexNode = this.head;
+        while(count != index){
+            indexNode = indexNode.next;
+            count++;
+        }
+        return indexNode;
+    }
 }
 const sll = new SinglyLikedList();
-console.log(sll.append(88));
-console.log(sll.append(768));
-console.log(sll.append(728));
+sll.append(88);
+sll.append(768);
+sll.append(7);
+sll.append(28);
+sll.remove(1);
 
+// console.log(sll.traverseAndReturnIndexNode(0))
+console.log(sll.pop());
